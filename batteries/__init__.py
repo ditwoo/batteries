4f4fece0aad276d3fa8eb3c831f99214080464bd
Dmytro Doroshenko <dimdoroshenko@gmail.com>
2020-08-05 19:56:14 +0300

    checkpoints average

--- batteries/__init__.py
+++ batteries/__init__.py
@@ -1,3 +1,3 @@
 from .checkpoints import CheckpointManager
 from .tensorboard import TensorboardLogger
-from .utils import seed_all, t2d, make_checkpoint
+from .utils import seed_all, t2d, make_checkpoint, checkpoints_weight_average
--- batteries/utils.py
+++ batteries/utils.py
@@ -2,6 +2,7 @@ import os
 import random
 import shutil
 from pathlib import Path
+from collections import OrderedDict
 from typing import Mapping, Any, Union, Sequence
 
 from packaging.version import parse, Version
@@ -85,6 +86,62 @@ def save_checkpoint(
     if is_last:
         last_filename = os.path.join(str(logdir), "last.pth")
         shutil.copyfile(filename, last_filename)
+
+
+def checkpoints_weight_average(*files) -> OrderedDict:
+    """Loads checkpoints from inputs and returns a model with averaged weights.
+
+    Args:
+        files: an iterable of string paths of checkpoints to load from.
+
+    Returns:
+        A dict of string keys mapping to various values. The 'model' key
+        from the returned dict should correspond to an OrderedDict mapping
+        string parameter names to torch Tensors.
+    """
+    params_dict = OrderedDict()
+    params_keys = None
+    new_state = None
+    num_models = len(files)
+
+    for f in files:
+        state = torch.load(
+            f,
+            map_location=(
+                lambda s, _: torch.serialization.default_restore_location(s, "cpu")
+            ),
+        )
+        # Copies over the settings from the first checkpoint
+        if new_state is None:
+            new_state = state
+
+        model_params = state["model_state_dict"]
+
+        model_params_keys = list(model_params.keys())
+        if params_keys is None:
+            params_keys = model_params_keys
+        elif params_keys != model_params_keys:
+            raise KeyError(
+                "For checkpoint {}, expected list of params: {}, "
+                "but found: {}".format(f, params_keys, model_params_keys)
+            )
+
+        for k in params_keys:
+            p = model_params[k]
+            if isinstance(p, torch.HalfTensor):
+                p = p.float()
+            if k not in params_dict:
+                params_dict[k] = p.clone()
+                # NOTE: clone() is needed in case of p is a shared parameter
+            else:
+                params_dict[k] += p
+
+    averaged_params = OrderedDict()
+    for k, v in params_dict.items():
+        averaged_params[k] = v
+        averaged_params[k].div_(num_models)
+    new_state["model_state_dict"] = averaged_params
+    return new_state
 
 
 def seed_all(seed: int = 42) -> None:
