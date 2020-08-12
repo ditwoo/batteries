3badcba407f664acd462c55ca4d648cc63dc8e4d
Dmytro Doroshenko <dimdoroshenko@gmail.com>
2020-08-12 22:57:51 +0300

    progress

--- examples/ddp/experiment.py
+++ examples/ddp/experiment.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import shutil
-from tqdm import tqdm
 from pathlib import Path
 import numpy as np
 
@@ -22,6 +21,14 @@ from batteries import (
     make_checkpoint,
     load_checkpoint,
 )
+from batteries.progress import tqdm
+
+
+######################################################################
+# TODOs:
+# 1. save tensorboard metrics after each step (train/valid)
+# 2. typings and docs to each function
+######################################################################
 
 
 def setup(rank, world_size):
@@ -44,46 +51,50 @@ def train_fn(
     optimizer,
     scheduler=None,
     accum_steps: int = 1,
-    verbose=True,
+    verbose: bool = True,
 ):
     model.train()
+
+    verbose_on_device = verbose and device == 0
 
     losses = []
     dataset_true_lbl = []
     dataset_pred_lbl = []
 
     to_iter = enumerate(loader)
-    if device == 0:
-        prbar = tqdm(to_iter, total=len(loader), file=sys.stdout, desc="train")
-    else:
-        prbar = to_iter
-    for _idx, (bx, by) in prbar:
-        bx, by = t2d((bx, by), device)
 
-        optimizer.zero_grad()
+    with tqdm(
+        total=len(loader), desc="train", disable=not verbose_on_device
+    ) as progress:
+        for _idx, (bx, by) in enumerate(loader):
+            bx, by = t2d((bx, by), device)
 
-        if isinstance(bx, (tuple, list)):
-            outputs = model(*bx)
-        elif isinstance(bx, dict):
-            outputs = model(**bx)
-        else:
-            outputs = model(bx)
+            optimizer.zero_grad()
 
-        loss = loss_fn(outputs, by)
-        _loss = loss.item()
-        losses.append(_loss)
-        loss.backward()
+            if isinstance(bx, (tuple, list)):
+                outputs = model(*bx)
+            elif isinstance(bx, dict):
+                outputs = model(**bx)
+            else:
+                outputs = model(bx)
 
-        dataset_true_lbl.append(by.flatten().detach().cpu().numpy())
-        dataset_pred_lbl.append(outputs.argmax(1).flatten().detach().cpu().numpy())
+            loss = loss_fn(outputs, by)
+            _loss = loss.item()
+            losses.append(_loss)
+            loss.backward()
 
-        if device == 0:
-            prbar.set_postfix_str(f"loss {_loss:.4f}")
+            dataset_true_lbl.append(by.flatten().detach().cpu().numpy())
+            dataset_pred_lbl.append(outputs.argmax(1).flatten().detach().cpu().numpy())
 
-        if (_idx + 1) % accum_steps == 0:
-            optimizer.step()
-            if scheduler is not None:
-                scheduler.step()
+            if verbose_on_device:
+                progress.set_postfix_str(f"loss {_loss:.4f}")
+
+            if (_idx + 1) % accum_steps == 0:
+                optimizer.step()
+                if scheduler is not None:
+                    scheduler.step()
+
+            progress.update(1)
 
     dataset_true_lbl = np.concatenate(dataset_true_lbl)
     dataset_pred_lbl = np.concatenate(dataset_pred_lbl)
@@ -92,19 +103,19 @@ def train_fn(
     return np.mean(losses), dataset_acc
 
 
-def valid_fn(model, loader, device, loss_fn):
+def valid_fn(model, loader, device, loss_fn, verbose: bool = True):
     model.eval()
+
+    verbose_on_device = verbose and device == 0
 
     losses = []
     dataset_true_lbl = []
     dataset_pred_lbl = []
-    with torch.no_grad():
+    with torch.no_grad(), tqdm(
+        total=len(loader), desc="valid", disable=not verbose_on_device
+    ) as progress:
         to_iter = loader
-        if device == 0:
-            prbar = tqdm(loader, file=sys.stdout, desc="valid")
-        else:
-            prbar = to_iter
-        for bx, by in prbar:
+        for bx, by in loader:
             bx, by = t2d((bx, by), device)
 
             if isinstance(bx, (tuple, list)):
@@ -117,11 +128,13 @@ def valid_fn(model, loader, device, loss_fn):
             loss = loss_fn(outputs, by).item()
             losses.append(loss)
 
-            if device == 0:
-                prbar.set_postfix_str(f"loss {loss:.4f}")
+            if verbose_on_device:
+                progress.set_postfix_str(f"loss {loss:.4f}")
 
             dataset_true_lbl.append(by.flatten().detach().cpu().numpy())
             dataset_pred_lbl.append(outputs.argmax(1).flatten().detach().cpu().numpy())
+
+            progress.update(1)
 
     dataset_true_lbl = np.concatenate(dataset_true_lbl)
     dataset_pred_lbl = np.concatenate(dataset_pred_lbl)
@@ -133,6 +146,10 @@ def valid_fn(model, loader, device, loss_fn):
 def experiment(rank, world_size, logdir: str):
     setup(rank, world_size)
     tb_logdir = logdir / "tensorboard"
+
+    def pprint(*args, **kwargs):
+        if rank == 0:
+            print(*args, **kwargs)
 
     seed_all()
     model = SimpleNet()
@@ -146,6 +163,7 @@ def experiment(rank, world_size, logdir: str):
 
     with TensorboardLogger(tb_logdir) as tb:
         stage = "stage0"
+        n_epochs = 10
 
         checkpointer = CheckpointManager(
             logdir=logdir / stage,
@@ -154,9 +172,8 @@ def experiment(rank, world_size, logdir: str):
             save_n_best=3,
         )
 
-        for ep in range(1, 10 + 1):
-            if rank == 0:
-                print(f"Epoch {ep}:")
+        for ep in range(1, n_epochs + 1):
+            pprint(f"[Epoch {ep}/{n_epochs}]")
             train_loss, train_acc = train_fn(
                 model, train_loader, rank, criterion, optimizer
             )
@@ -187,16 +204,16 @@ def experiment(rank, world_size, logdir: str):
                     ),
                 )
 
-                print(f"            train loss - {train_loss:.5f}")
-                print(f"train dataset accuracy - {train_acc:.5f}")
-                print(f"            valid loss - {valid_loss:.5f}")
-                print(f"valid dataset accuracy - {valid_acc:.5f}")
-                print()
+            pprint(f"            train loss - {train_loss:.5f}")
+            pprint(f"train dataset accuracy - {train_acc:.5f}")
+            pprint(f"            valid loss - {valid_loss:.5f}")
+            pprint(f"valid dataset accuracy - {valid_acc:.5f}")
+            pprint()
 
         # do a next training stage
         stage = "stage1"
-        if rank == 0:
-            print(f"\n\nStage - {stage}")
+        n_epochs = 10
+        pprint(f"\n\nStage - {stage}")
 
         dist.barrier()
         model = SimpleNet()
@@ -213,9 +230,8 @@ def experiment(rank, world_size, logdir: str):
             save_n_best=3,
         )
 
-        for ep in range(1, 10 + 1):
-            if rank == 0:
-                print(f"Epoch {ep}:")
+        for ep in range(1, n_epochs + 1):
+            pprint(f"[Epoch {ep}/{n_epochs}]")
             train_loss, train_acc = train_fn(
                 model, train_loader, rank, criterion, optimizer
             )
@@ -246,11 +262,11 @@ def experiment(rank, world_size, logdir: str):
                     ),
                 )
 
-                print(f"            train loss - {train_loss:.5f}")
-                print(f"train dataset accuracy - {train_acc:.5f}")
-                print(f"            valid loss - {valid_loss:.5f}")
-                print(f"valid dataset accuracy - {valid_acc:.5f}")
-                print()
+            pprint(f"            train loss - {train_loss:.5f}")
+            pprint(f"train dataset accuracy - {train_acc:.5f}")
+            pprint(f"            valid loss - {valid_loss:.5f}")
+            pprint(f"valid dataset accuracy - {valid_acc:.5f}")
+            pprint()
 
     cleanup()
 
