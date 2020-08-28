a12d462a80389216a36a5c5d1d53763836859ec7
Dmytro Doroshenko <dimdoroshenko@gmail.com>
2020-08-28 22:04:06 +0300

    renamed iteration variable

--- batteries/mixup.py
+++ batteries/mixup.py
@@ -39,7 +39,7 @@ class Mixup:
             mixup_lambdas: (batch_size,)
         """
         mixup_lambdas = []
-        for n in range(0, batch_size, 2):
+        for _ in range(0, batch_size, 2):
             lam = self.random_state.beta(self.mixup_alpha, self.mixup_alpha, 1)[0]
             mixup_lambdas.append(lam)
             mixup_lambdas.append(1.0 - lam)
