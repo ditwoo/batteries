418016ed492e45144a5dc5d6536ceae31c12a11e
Dmytro Doroshenko <dimdoroshenko@gmail.com>
2020-11-22 03:15:39 +0200

    write images wrapper

--- batteries/tensorboard.py
+++ batteries/tensorboard.py
@@ -1,22 +1,47 @@
 from pathlib import Path
-from typing import Union, Mapping
+from typing import Mapping, Union
+
 from torch.utils.tensorboard import SummaryWriter
 
 
 class TensorboardLogger:
-    def __init__(self, logdir: Union[str, Path]):
+    """Tensorboard wrapper.
+
+    Args:
+        logdir (str or pathlib.Path): directory where should be stored logs.
+    """
+
+    def __init__(self, logdir):  # noqa: D107
         self.writer = SummaryWriter(log_dir=logdir, max_queue=1)
 
     def __enter__(self):
         return self
 
-    def metric(
-        self, plot_name: str, value: Union[float, Mapping[str, float]], iteration: int
-    ) -> None:
+    def metric(self, name, value, iteration):
+        """Write metric using tensorboard writer.
+
+        Args:
+            name (str): name to use for metric values
+            value (float or Dict[str, float]): metic to store,
+                if passed dict with metics then they will be
+                stored with name prefix.
+            iteration (int): iteration number (batch/epoch index or similar)
+        """
         if isinstance(value, (int, float)):
-            self.writer.add_scalar(plot_name, value, iteration)
+            self.writer.add_scalar(name, value, iteration)
         elif isinstance(value, dict):
-            self.writer.add_scalars(plot_name, value, iteration)
+            self.writer.add_scalars(name, value, iteration)
+
+    def images(self, name, value, iteration):
+        """Write images using tensorboard writer.
+
+        Args:
+            name (str): name to use for images
+            value (np.ndarray or torch.tensor): tensor with images,
+                should have shapes - (B)x(C)x(H)x(W)
+            iteration (int): iteration number (batch/epoch index or similar)
+        """
+        self.writer.add_images(name, value, iteration)
 
     def __exit__(self, ex_type, ex_value, ex_traceback):
         self.writer.close()
@@ -24,6 +49,7 @@ class TensorboardLogger:
 
 if __name__ == "__main__":
     import shutil
+
     import numpy as np
 
     logdir = "./tb_check"
